3ab6911ed24baf890f584e8be9db5945ee0b8a9f
morg-us <115581488+morg-us@users.noreply.github.com>
2026-01-13 02:09:27 +0300

    Update GameStateManager.cs

--- Assets/Scripts/Core/GameStateManager.cs
+++ Assets/Scripts/Core/GameStateManager.cs
@@ -1,1 +1,28 @@
+using UnityEngine;
+
+public enum GameState
+{
+    MainMenu,
+    Playing,
+    Paused
+}
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+    public GameState CurrentState;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void SetState(GameState newState)
+    {
+        CurrentState = newState;
+    }
+}
 
