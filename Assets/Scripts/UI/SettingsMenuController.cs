7e6d716cf537865de64bda273d6575f71a539a0c
morg-us <115581488+morg-us@users.noreply.github.com>
2026-01-13 02:10:25 +0300

    Add settings methods for brightness, volume, and graphics

--- Assets/Scripts/UI/SettingsMenuController.cs
+++ Assets/Scripts/UI/SettingsMenuController.cs
@@ -1,1 +1,25 @@
+using UnityEngine;
+
+public class SettingsMenuController : MonoBehaviour
+{
+    public void SetBrightness(float value)
+    {
+        Debug.Log("Brightness: " + value);
+    }
+
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void SetSensitivity(float value)
+    {
+        PlayerPrefs.SetFloat("Sensitivity", value);
+    }
+
+    public void SetGraphics(int level)
+    {
+        QualitySettings.SetQualityLevel(level);
+    }
+}
 
