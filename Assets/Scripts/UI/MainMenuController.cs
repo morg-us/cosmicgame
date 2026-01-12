f15b09eb951a4afb113a11c3eaa732b033888425
morg-us <115581488+morg-us@users.noreply.github.com>
2026-01-13 02:08:24 +0300

    Add MainMenuController with game menu functions

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,1 +1,20 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnStartGame()
+    {
+        Debug.Log("Oyun Başlıyor...");
+    }
+
+    public void OnMarket()
+    {
+        Debug.Log("Market Açıldı");
+    }
+
+    public void OnInventory()
+    {
+        Debug.Log("Envanter Açıldı");
+    }
+}
 
