479988c0463a04c0b214c0bbcd4d4d9cbc7b6599
morg-us <115581488+morg-us@users.noreply.github.com>
2026-01-13 02:12:37 +0300

    Implement ProfileUIController for player profile display

--- Assets/Scripts/UI/ProfileUIController.cs
+++ Assets/Scripts/UI/ProfileUIController.cs
@@ -1,1 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileUIController : MonoBehaviour
+{
+    public Text playerNameText;
+    public Text levelText;
+    public Slider xpBar;
+
+    public PlayerProfile profile;
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        playerNameText.text = profile.playerName;
+        levelText.text = "Level " + profile.level;
+        xpBar.value = profile.experience;
+    }
+}
 
